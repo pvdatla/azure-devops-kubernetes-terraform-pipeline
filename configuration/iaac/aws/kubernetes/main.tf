bbb456fcc3fcdf682595b23d1b0b82989d733d72
Pradeep Datla <pradeepdatla@Pradeeps-MBP.lan>
2025-04-21 18:31:50 -0500

    added user auth

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -85,6 +85,11 @@ resource "kubernetes_cluster_role_binding" "example" {
     name      = "cluster-admin"
   }
   subject {
+    kind      = "User"
+    name      = "admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+  subject {
     kind      = "ServiceAccount"
     name      = "default"
     namespace = "default"
